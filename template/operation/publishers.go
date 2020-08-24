07ad32b0ebef0ca73fef62cfce89741a38ade282
Jacob Poston <dev@jacobposton.com>
2020-08-23 23:06:05 -0400

    Generate publishing interface for channel operations

---
+++ template/operation/publishers.go
@@ -0,0 +1,19 @@
+{%- from "../../partials/go.template" import messageName -%}
+package operation
+
+type Producer interface {
+
+{%- for ch_name, ch in asyncapi.channels() -%}
+	{%- if ch.hasPublish()  -%}
+		{%- set opName = ch.publish().id() | toGoPublicID -%}
+		{%- set msgName = messageName(ch.publish().message()) %}
+
+	// {{opName}} publishes a {{msgName}} message to the transport implementation using the
+	// params provided in the param.{{opName}}Params. It is bound to the following channel operation:
+	//
+	// PUBLISH: {{ch_name}} 
+	func {{ ch.publish().id() | toGoPublicID }}(params param.{{opName}}Params, msg message.{{msgName}})
+
+	{%- endif -%}
+{% endfor %}
+}
