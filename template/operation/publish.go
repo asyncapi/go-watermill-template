15d23bdea84f41edef87e66b1b1f5208ff47854c
Jacob Poston <dev@jacobposton.com>
2020-08-23 23:33:57 -0400

    Move & cleanup Producer interface

--- template/operation/publish.go
+++ template/operation/publish.go
@@ -8,8 +8,8 @@ type Producer interface {
 		{%- set opName = ch.publish().id() | toGoPublicID -%}
 		{%- set msgName = messageName(ch.publish().message()) %}
 
-	// {{opName}} publishes a {{msgName}} message to the transport implementation using the
-	// params provided in the param.{{opName}}Params. It is bound to the following channel operation:
+	// {{opName}} publishes a {{msgName}} message using the params provided 
+	// in the param.{{opName}}Params. It is bound to the following channel:
 	//
 	// PUBLISH: {{ch_name}} 
 	func {{ ch.publish().id() | toGoPublicID }}(params param.{{opName}}Params, msg message.{{msgName}})
