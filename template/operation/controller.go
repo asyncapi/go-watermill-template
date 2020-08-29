5f7e6b7f39b3552884b5e45b5d04d53506347a94
Jacob Poston <dev@jacobposton.com>
2020-08-29 15:46:09 -0400

    Apply operation bindings to transport pubsub calls

--- template/operation/controller.go
+++ template/operation/controller.go
@@ -16,18 +16,34 @@ type Controller {
 		{%- set msgName = messageName(ch.publish().message()) -%}
 
 // {{ ch.publish().id() | toGoPublicID }} implements operation.Producer
-func (c Controller) {{ ch.publish().id() | toGoPublicID }}(params channel.{{opName}}Params, msg message.{{msgName}}) {
+func (c Controller) {{ ch.publish().id() | toGoPublicID }}(params channel.{{opName}}Params, msg message.{{msgName}}) error {
 	// Define any operation bindings. These are constant per operation
+	{%- set bindingList = "" -%}
+	{%- set counter = 0 -%}
 	{%- for protocol, binding in  ch.publish().bindings() -%}
 		{{ getOpBinding(protocol, binding) }}
+
+		{#- Append bindings variable name to a string of operation bindings (used in Subscribe below) -#}
+		{%- if counter  < ch.publish().bindings() | length  -%}
+			{%- set bindingList = bindingList + ", " -%}
+		{% endif -%}
+		{%- set bindingList = bindingList + protocol + "Bindings" -%}
+		{%- set counter = counter + 1 %}
 	{%- endfor %}
 
-	// Handle no writer? Throw error
+	// Throw error for missing content type encoder
 	w := c.getWriter(msg.ContentType)
-	buf := w.Write(msg.Payload)
-	ch := channel.Publish{{opName}}.Build(params)
+	if w == nil {
+		return errors.New("no message writer is registered for content type: " + msg.ContentType)
+	}
 
-	c.async.Publish(ch, b, bindingMQTT5, bindingMQTT5)
+	// Throw error if failed to encode payload
+	if msg.RawPayload, err := w.Write(msg.Payload) {
+		return err
+	}
+
+	// Publish the underlying transport.Message with the transport layer
+	return c.async.Publish(params.Build(), mag.Message{{bindingList}})
 }
 
 	{% else  -%}
@@ -36,24 +52,42 @@ func (c Controller) {{ ch.publish().id() | toGoPublicID }}(params channel.{{opNa
 		{%- set msgName = messageName(ch.subscribe().message()) -%}
 
 // {{ ch.subscribe().id() | toGoPublicID }} implements operation.Consumer
-func (c Controller) {{ ch.subscribe().id() | toGoPublicID }}(params channel.{{opName}}Params, fn {{msgName}}Handler) {
+func (c Controller) {{ ch.subscribe().id() | toGoPublicID }}(params channel.{{opName}}Params, fn {{msgName}}Handler) error {
 	// Define any operation bindings. These are constant per operation
+	{%- set bindingList = "" -%}
+	{%- set counter = 0 -%}
 	{%- for protocol, binding in  ch.subscribe().bindings() -%}
 		{{ getOpBinding(protocol, binding) }}
+
+		{#- Append bindings variable name to a string of operation bindings (used in Subscribe below) -#}
+		{%- if counter < ch.subscribe().bindings() | length  -%}
+			{%- set bindingList = bindingList + ", " -%}
+		{% endif -%}
+		{%- set bindingList = bindingList + protocol + "Bindings" -%}
+		{%- set counter = counter + 1 %}
 	{%- endfor %}
 
-	// Handle no reader? Throw error
+	// Throw error before subscribing if expected content type reader is not registered
 	r := c.getReader(msg.ContentType)
-	c.async.Subscribe(ch, func(topic, buf, {bindings})) {
+	if r == nil {
+		return errors.New("no message writer is registered for content type: " + msg.ContentType)
+	}
+
+	// Subscribe with the transport layer. Wrap message handler /w logic to decode 
+	// transport.Message payload into {{msgName}} message
+	c.async.Subscribe(ch, func(topic, buf{{bindingList}})) {
 		// Init a new message object & attempt to use the defined content.Reader to parse
 		// the message payload
 		msg := message.New{{msgName}}()
 		if err := r.Read(buf, &msg.Payload); err != nil {
-			// TODO: Handle error
+			return error
 		}
 
 		// Parse the params out of the channel
-		recParams := channel.Subscribe{{opName}}.Parse(ch)
+		recParams := &channel.{{opName}}Params{}
+		if err := recParams.Parse(ch); err != nil {
+			return err
+		}
 
 		// Call the operation's message handler
 		fn(recParams, msg)
