6cae86cf0fbb1a290811ac510b907c14d8b90cdb
Jacob Poston <dev@jacobposton.com>
2020-08-29 00:30:21 -0400

    Segment message at transportation layer

---
+++ template/transport/message.go
@@ -0,0 +1,39 @@
+package transport
+
+import "context"
+
+// Message contains the payload and headers sent or received from the 
+// transport implementation
+type Message {
+	// context holds the context application context of this message
+	// it can be used to pass tracing information & signal timeouts for pub/sub operations
+	context context.Context
+
+	// Headers contain the custom header values sent/received alongside a message payload
+	Headers map[string]string
+
+	// RawPayload contains the serialized binary payload of the message
+	RawPayload []byte
+}
+
+// NewMessage instantiates a new message with the provided context
+func NewMessage(ctx context.Context) Message {
+	ctx := context.Background()
+
+	return Message{
+		Headers: make(map[string]string)
+		RawPayload: nil
+	}
+}
+
+// Context returns the associated message Context
+func (msg Message) Context() context.Context {
+	return msg.context
+}
+
+
+// WithContext returns a new instance of msg with the provided context
+func (msg Message) WithContext(ctx context.Context) Message {
+	msg.context = ctx
+	return msg
+}
