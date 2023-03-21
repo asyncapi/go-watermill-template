a8f01bec4bd56d565f1cd6408bc145aed4c7c139
Jonas Lagoni <jonas-lt@live.dk>
2023-03-21 10:42:41 +0100

    fix: modelina payload generation

--- template/asyncapi/payload.js
+++ template/asyncapi/payload.js
@@ -5,10 +5,8 @@ export default async function({ asyncapi }) {
   const generator = new GoGenerator({ presets: [
     {
       struct: {
-        field({ fieldName, field, renderer, type }) {
-          const formattedFieldName = renderer.nameField(fieldName, field);
-          const fieldType = renderer.renderType(field);
-          return `${ formattedFieldName } ${ fieldType } \`json:"${ fieldName }"\``;
+        field({ field }) {
+          return `${ field.propertyName } ${ field.property.type } \`json:"${ field.unconstrainedPropertyName }"\``;
         },
       }
     }
